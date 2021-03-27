a8d7c112c337385e3733f3eb606b983b5a471a91
cw-johnson <70862362+cw-johnson@users.noreply.github.com>
2021-03-27 00:50:17 -0400

    Add files via upload

--- CurrentControlTest/testCurrentControlPID.ino
+++ CurrentControlTest/testCurrentControlPID.ino
@@ -1,22 +1,37 @@
-#include "motorDriver.h"
+#include "motorDriver.h" //Make sure to have this file and the .cpp in your project folder
 #include <Wire.h>
-#include <Adafruit_INA219.h>
+#include "Adafruit_INA219.h" 
+//Holy moley this is actually kinda working
 
 Adafruit_INA219 ina219; //Create Current Sensor Object
 
 motorDriver motor1; //Create motorDriver object
 
+
+#define REFRESH_PERIOD 0.02 //In s. Corresponds to sampling rate of 50Hz
+#define MOTOR_THRESHOLD 10 //Any output below this percent will have the motor free spin
 int i = 0;
 elapsedMillis sinceRead; 
 float curerr=0;
 float lasterr=0;
 float deriv=0; 
 float integ=0;
-float descur=200; //Our setpoint, in mA. "Maintain 200mA"
+float descur=150; //Our setpoint, in mA. "Maintain 200mA"
 float outp=0;
-int kp = 1;  //Proportional constant
-int kd = 1;  //Derivative constant
-int ki = 0;  //Integral constant
+
+float shuntvoltage = 0;
+float busvoltage = 0;
+float current_mA = 0;
+float loadvoltage = 0;
+float power_mW = 0;
+
+//PID Controller Constants
+float kp = 0.001;  //Proportional constant
+float kd = 0.00000000001;  //Derivative constant
+float ki = 0.3;  //Integral constant
+float kc = 1.0; //Overall Controller Gain
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -39,7 +54,7 @@ void loop() {
   float num1f = Serial.parseFloat();
   float num2f = Serial.parseFloat();
   int num1 = num1f;
-  int num2 = num2f;
+  int num2 s= num2f;
   num2 = num2 % 256;
   Serial.print("Mode: ");
   Serial.println(num1);
@@ -47,53 +62,54 @@ void loop() {
   Serial.println(num2);
   motor1.spinMotor(num1,num2);
   */
-
+ 
+  if (sinceRead >= REFRESH_PERIOD*1000){ //Read values when timer reaches 100ms, then reset timer to zero
+    sinceRead=0;
+    shuntvoltage = ina219.getShuntVoltage_mV();
+    busvoltage = ina219.getBusVoltage_V();
+    current_mA = ina219.getCurrent_mA();
+    power_mW = ina219.getPower_mW();
+    loadvoltage = busvoltage + (shuntvoltage / 1000);
   
+    //Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+    //Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
+    //Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+    //Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+    //Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+    
 
-  while (sinceRead < 100){} //This code acts like a timer/interrupt, waits 100ms to read current sensor, and drive motor
-  sinceRead=0;              //Note: Minimum delay value is around ~65ms due to our sensor, in it's configuration rn
-
+    curerr = descur - current_mA; //Calculate proportional error
+    deriv = (curerr - lasterr)/REFRESH_PERIOD; //Calculate derivative error
+    integ += curerr*REFRESH_PERIOD;   //Calculate integral error
   
-  float shuntvoltage = 0;
-  float busvoltage = 0;
-  float current_mA = 0;
-  float loadvoltage = 0;
-  float power_mW = 0;
+    outp = kc*(curerr*kp + deriv*kd + integ*ki); //Output is each error scaled by corresponding k
   
-  shuntvoltage = ina219.getShuntVoltage_mV();
-  busvoltage = ina219.getBusVoltage_V();
-  current_mA = ina219.getCurrent_mA();
-  power_mW = ina219.getPower_mW();
-  loadvoltage = busvoltage + (shuntvoltage / 1000);
+    lasterr = curerr; //Save error for next run for derivative term
 
-  //Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
-  //Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
-  //Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
-  //Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
-  //Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
-  Serial.println(current_mA);
-
-  curerr = descur - current_mA; //Calculate proportional error
-  deriv = (curerr - lasterr)/(0.1); //Calculate derivative error
-  integ += curerr/0.1;   //Calculate integral error
-  outp = curerr*kp + deriv*kd + integ*ki; //Output is each error scaled by corresponding k
-  
-  lasterr = curerr; //Save error for next run for derivative term
-  if (outp>0.0){ //If output positive, spin clockwise
-    if (outp>100.0){
+    if (outp>100.0)
       outp=100.0;
+    else if(outp<-100.0)
+      outp=-100.0;
+ 
+    //erial.println(curerr);
+    //Serial.print(",");
+    //Serial.println(deriv);
+    //Serial.print(",");
+    //Serial.println(integ);
+    //Serial.print(",");
+    Serial.println(current_mA);
+    Serial.print(",");
+    Serial.println(outp);
+
+    if (outp>(MOTOR_THRESHOLD)) //If output positive, spin clockwise
+      motor1.spinMotor(FORWARD,outp);
+    else if (outp<(-MOTOR_THRESHOLD)){  //If output negative, counter clockwise
+      outp=-outp;
+      motor1.spinMotor(REVERSE,outp);
     }
-    motor1.spinMotor(0,outp);
-  }
-  else{  //Else spin counter clockwise
-    outp=-outp;
-    if (outp>100.0){
-      outp=100.0;
+    else{ //Motor is in "dead zone", free stop
+      motor1.spinMotor(FREESTOP,0);
     }
-    motor1.spinMotor(1,outp);
+
   }
-  //Maybe there should be a case for "coasting?", ie freestop mode
-  
-  Serial.print(",");   //We can print 2 values to serial monitor by doing this
-  Serial.println(outp);
 }
