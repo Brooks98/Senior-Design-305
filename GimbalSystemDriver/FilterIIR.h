2873d50c9121b06a5ec761483eaae51d2b15809c
cw-johnson <70862362+cw-johnson@users.noreply.github.com>
2021-03-31 16:27:17 -0400

    Update FilterIIR.h

--- GimbalSystemDriver/FilterIIR.h
+++ GimbalSystemDriver/FilterIIR.h
@@ -3,22 +3,25 @@ class FilterIIR{
   public:
     FilterIIR();
     
-    float getFilteredData(float _newval);
+    float filter(float _incoming);
     int setStages(int _stages);
     int setConstant(float _a);
     
     int stages;
     float a;
-    float lastval, newval;
+    float lastval[4];
+    float newval;
 };
 
 FilterIIR::FilterIIR(){
   a = 0.8;
-  lastval = newval = 0;
+  lastval1 = lastval2 = lastval3 = lastval4 = newval = 0;
  }
  
-float getFilteredData(float _newval)[
-  
+float filter(float _incoming){
+  for (int i = 0; i<stages; i++)
+    lastval[i] = newval = (1-a)*incoming + a*lastval[i];  
+  return newval;
 }
 
 int setStages(int _stages){
