446f6b68cb801b4adbad3fea1aaf660497cbfc40
Teresa Weaver <teresa.weaver1027@gmail.com>
2021-04-14 23:02:56 -0400

    Add files via upload
    Final Code

---
+++ ControllerMainForward.ino
@@ -0,0 +1,247 @@
+//Encoder *********************************
+#include <Encoder.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+Encoder motorleft(2, 3); //(2,3)
+Encoder motorright(33,34);
+long positionLeft  = -999;
+
+int in_pin1 = 20;
+int in_pin2 = 19;
+int enable_pinL = 37;
+
+int in_pin3 = 17;
+int in_pin4 = 16;
+int enable_pinR = 18;
+
+int forwardmax = 50;
+int backwardmax = -50;
+int centerleft = 0;
+int currentleft;
+
+int rightmax = -50;
+int leftmax = 50;
+int centerright = 0;
+int currentright;
+//****************************************
+
+//Radio *********************************
+RF24 radio(8, 7); // CE, CSN
+const byte addresses [][6] = {"007", "001"};    //Setting the two addresses. One for transmitting and one for receiving
+int distance = 0;
+//***************************************
+
+//Info to send to vehicle ******************
+int data[4] = {0,0,0,0}; //data[0] = speed, data[1] = turning
+int speedy; //negative is backwards, forward in positive
+int turn;
+//******************************************
+
+
+void setup() {
+//  Serial.begin(9600);
+//  Serial.println("Setup");
+
+  radio.begin();                            //Starting the radio communication
+  radio.openWritingPipe(addresses[0]);      //Setting the address at which we will send the data
+  radio.openReadingPipe(1, addresses[1]);   //Setting the address at which we will receive the data
+  radio.setPALevel(RF24_PA_MAX);            //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  //radio.setDataRate( RF24_250KBPS );
+  //radio.setRetries(1,15); // delay, count
+  //initialvalues(); //calculates center encoder value at startup
+  //recenter();
+  
+  pinMode(enable_pinL, OUTPUT);
+  pinMode(enable_pinR, OUTPUT);
+  pinMode(in_pin1, OUTPUT);
+  pinMode(in_pin2, OUTPUT);
+  pinMode(in_pin3, OUTPUT);
+  pinMode(in_pin4, OUTPUT);
+ 
+  
+}
+void loop()
+{ 
+
+
+//  delay(100);
+  // Gets encoder values **********
+  int newLeft;
+  newLeft = motorleft.read();
+  if (newLeft != currentleft) {
+
+    currentleft = newLeft;
+  }
+
+  int newRight;
+  newRight = motorright.read();
+  if(newRight != currentright)
+  {
+    currentright = newRight;
+  }
+
+  //*****************************
+
+  //Calculates speed & direction ********
+  //speedy = (currentleft - centerleft)* ((200)/(abs(forwardmax)+abs(backwardmax)));
+  speedy = currentleft;
+  data[1] = speedy;
+  turn = currentright;
+  data[0] = turn;
+  //*************************************
+ 
+  delay(10);// increase maybe? 
+  radio.stopListening(); 
+  radio.write(data, sizeof(data));
+  delay(50); 
+  radio.startListening();                    //This sets the module as receiver
+ 
+  if (radio.available())                     //Looking for incoming data
+  {
+
+//    Serial.print("Left Encoder = ");
+//    Serial.print(currentleft);
+//    Serial.print("\tSpeedy = ");
+//    Serial.print(speedy);
+//    Serial.print("\tRight Encoder = ");
+//    Serial.print(currentright);
+//    Serial.print("\t");
+    
+    radio.read(&distance, sizeof(distance)); // reading in data for distance sensor
+//    Serial.print("Distance: ");
+//    Serial.print(distance);
+//    Serial.println(" cm"); 
+  }
+
+
+   
+//**********************************************************
+ if(currentleft > (centerleft + 5) && (distance < 30))
+    {
+      
+      int joystickspeed;
+      if (distance > 3)
+      {
+        joystickspeed = map(distance, 0, 30, 210, 185);
+      }
+      else
+      {
+        joystickspeed = 210;
+      }
+
+      leftbackward(joystickspeed);
+       
+    }
+    else
+    {
+      digitalWrite(in_pin1, LOW);
+      digitalWrite(in_pin2, LOW);
+      digitalWrite(in_pin3, LOW);
+      digitalWrite(in_pin4, LOW);
+//      recenterleft();
+    }
+       recenterright();
+}
+
+void initialvalues(){
+
+  leftbackward(255);
+  delay(1000);
+ 
+  leftforward(255);
+  delay(1000);
+  forwardmax = motorleft.read();
+  delay(300);
+ 
+  leftbackward(255);
+  delay(1000);
+  backwardmax = motorleft.read();
+  delay(300);
+
+  leftstop();
+ 
+  centerleft = (forwardmax+backwardmax)/2;
+//  Serial.print("forwardmax = ");
+//  Serial.print(forwardmax);
+//  Serial.print("\n");
+//  Serial.print("backwardmax = ");
+//  Serial.print(backwardmax);
+//  Serial.print("\n");
+//  Serial.print("center = ");
+//  Serial.print(centerleft);
+//  Serial.print("\n");
+}
+void leftforward(int sp)
+{
+  analogWrite(enable_pinL, sp);
+  digitalWrite(in_pin1, HIGH);
+  digitalWrite(in_pin2, 0);
+}
+void leftbackward(int sp)
+{
+  analogWrite(enable_pinL, sp);
+  digitalWrite(in_pin1, 0);
+  digitalWrite(in_pin2, HIGH);
+}
+void leftstop()
+{
+  analogWrite(enable_pinL, 0);
+  digitalWrite(in_pin1, 0);
+  digitalWrite(in_pin2, 0);
+}
+void rightright(int sp)
+{
+  analogWrite(enable_pinR, sp);
+  digitalWrite(in_pin3, HIGH);
+  digitalWrite(in_pin4, 0);
+}
+void rightleft(int sp)
+{
+  analogWrite(enable_pinR, sp);
+  digitalWrite(in_pin3, 0);
+  digitalWrite(in_pin4, HIGH);
+}
+void rightstop()
+{
+  analogWrite(enable_pinR, 0);
+  digitalWrite(in_pin3, 0);
+  digitalWrite(in_pin4, 0);
+}
+void recenterleft(){
+    if (currentleft < centerleft - 7)
+    {
+      leftforward(175);
+    }
+    else if (currentleft > centerleft + 7)
+    {
+       leftbackward(175);
+    }
+    else
+    {
+      leftstop();
+    }
+}
+void recenterright(){
+    if (currentright < centerright - 5)
+    {
+      int sp = constrain(currentright , -5, -40);
+      sp = map(sp, -5,-40, 155, 230);
+      
+      rightleft(sp);
+
+    }
+    else if(currentright > centerright + 5)
+    {
+      int sp = constrain(currentright , 5, 40);
+      sp = map(sp, 5,40, 155, 170);
+      
+      rightright(sp); 
+    }
+    else
+    {
+      rightstop();
+    }
+
+}
---
+++ VehicleMainForward.ino
@@ -0,0 +1,162 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+#define echoPin 0 // HC-SR04 echo pin to D4 
+#define trigPin 1 //HC-SR04 trig pin to D5 (PWM)
+double maxspeed = 512;
+
+// Define variables for radio
+RF24 radio(9, 10); // CE to pin D9, CSN to pin D10
+const byte addresses [][6] = {"007", "001"};  //Setting the two addresses. One for transmitting and one for receiving
+
+// declaring array for encoder values to recieve info
+int data[4]={0,0,0,0};// data[0]=speed, data[1]= direction
+
+//servo
+Servo dirServo;                   // define servo to control turning of smart car
+int dirServoPin = 2;              // define pin for signal line of the last servo
+float dirServoOffset = -2;         // define a variable for deviation(degree) of the servo
+int turn = 0;
+
+// Define variables for HC-SR04
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+const int dirAPin = 7;    // define pin used to control rotational direction of motor A
+const int pwmAPin = 6;    // define pin for PWM used to control rotational speed of motor A
+const int dirBPin = 4;    // define pin used to control rotational direction of motor B
+const int pwmBPin = 5;    // define pin for PWM used to control rotational speed of motor B
+const int RPin = A3;
+const int GPin = A4;
+const int BPin = A5;
+int RGBVal = 0;
+
+#define FORWARD HIGH
+#define BACKWARD LOW
+ int speedy;
+// int turn;
+ bool directionleft;
+
+void setup() {
+  // void setup for radio and HC-SR04
+  pinMode(dirAPin, OUTPUT);   // set dirAPin to output mode
+  pinMode(pwmAPin, OUTPUT);   // set pwmAPin to output mode
+  pinMode(dirBPin, OUTPUT);   // set dirBPin to output mode
+  pinMode(pwmBPin, OUTPUT);   // set pwmBPin to output mode
+  pinMode(RPin, OUTPUT);   // set RPin to output mode
+  pinMode(GPin, OUTPUT);   // set GPin to output mode
+  pinMode(BPin, OUTPUT);   // set BPin to output mode
+ 
+ pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+ pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+//  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+//  Serial.println("vehicle code startup"); // print some text in Serial Monitor
+// 
+  radio.begin();                           //Starting the radio communication
+  radio.openWritingPipe(addresses[1]);     //Setting the address at which we will send the data
+  radio.openReadingPipe(1, addresses[0]);  //Setting the address at which we will receive the data
+  radio.setPALevel(RF24_PA_MAX); //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  //radio.setDataRate( RF24_250KBPS );
+//  radio.setRetries(0, 15);  
+  radio.setRetries(1, 15);  
+  //servo for turning
+  dirServo.attach(dirServoPin);  // attaches the servo on servoDirPin to the servo object
+ 
+  radio.startListening();
+ 
+}
+
+ 
+
+void loop()
+{  // HC-SR04
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+//  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+//  // Displays the distance on the Serial Monitor
+//  Serial.print("Distance: ");
+//  Serial.print(distance);
+//  Serial.println(" cm\t");
+
+
+  //// Radio code
+  delay(10);
+  radio.startListening();   //This sets the module as receiver
+  //delay(5);
+  if (radio.available())                     //Looking for incoming data
+  {
+    //digitalWrite(RPin, LOW);digitalWrite(GPin, HIGH);digitalWrite(BPin, LOW); //pretty colors
+    //radio.read(&speedy, sizeof(speedy));
+    //radio.read(&turn, sizeof(turn));
+    radio.read(data, sizeof(data));
+    speedy=data[2];
+    turn=data[0];
+    double speedy2 = speedy*1.0;
+//    Serial.print("Speed = ");
+//    Serial.print(speedy);
+//    Serial.print("\t");
+//    Serial.print("Speed2 = ");
+//    Serial.print(speedy2);
+//    Serial.print("\t");
+    if(speedy >= 0)
+    {
+      directionleft = FORWARD;
+    }
+    else
+    {
+      directionleft = BACKWARD;
+    }
+    double newspeed = 2.0*(speedy2*maxspeed)/100.0;
+    newspeed = abs(constrain(newspeed, -512, 512));
+    newspeed = map(newspeed, 0, 512, 0, 200);
+//    Serial.print("NewSpeed: ");
+//    Serial.print(newspeed);
+//    Serial.print("\t");
+   
+    // calculate the steering angle of servo according to the direction joystick of remote control and the deviation
+//    Serial.print("Data[0]: ");
+//    Serial.print(data[0]);
+//    Serial.print("\t");
+//    Serial.print("Data[2]: ");
+//    Serial.print(data[2]);
+//    Serial.print("\n");
+
+   
+    turn = constrain(turn, -40, 40);
+    turn = map(turn, -40, 40, 45 ,135 );
+//    Serial.print("Servo degree: ");
+//    Serial.print(turn);
+//    Serial.print("\n");
+   
+    digitalWrite(dirAPin, directionleft);
+    digitalWrite(dirBPin, directionleft);
+    analogWrite(pwmAPin, newspeed);
+    analogWrite(pwmBPin, newspeed);
+    dirServo.write(turn + dirServoOffset);
+  }
+
+  //delay(5);
+  delay(10); 
+  radio.stopListening();                             //This sets the module as transmitter
+
+  radio.write(&distance, sizeof(distance)); // Sending data for the distance sensor
+
+  if (distance <= 20)
+  {
+    digitalWrite(RPin, LOW);digitalWrite(GPin, HIGH);digitalWrite(BPin, LOW); //pretty colors
+  }
+  else
+  {
+    digitalWrite(RPin, HIGH);digitalWrite(GPin, LOW);digitalWrite(BPin, HIGH); //pretty colors
+  }
+  //delay(5);
+}
